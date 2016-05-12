136dcf5577e87e51e7215012e82cce84af445b8d
nv-vn <nv@cock.li>
2016-05-11 20:36:24 -0400

    Update for TelegraML 2.0.0

--- src/bot.ml
+++ src/bot.ml
@@ -16,11 +16,13 @@ module MyBot = Api.Mk (struct
     open Api.Message
     open Api.Audio.Out
 
+    include BotDefaults
+
     let token = [%blob "../bot.token"] (* Remember, we start from _build/ *)
     let rec commands =
       let open Lwt in
       let greet = function
-        | {chat; message_id} as msg -> SendMessage (chat.id, "Hello, " ^ get_sender msg, Some message_id, None) in
+        | {chat; message_id} as msg -> SendMessage (chat.id, "Hello, " ^ get_sender msg, false, Some message_id, None) in
       let meow_cache = ref [] in
       let meow = function
         | {chat} -> begin
@@ -31,19 +33,19 @@ module MyBot = Api.Mk (struct
             else ();
             let len = List.length !meow_cache in
             if len = 0 then
-              SendMessage (chat.id, "Couldn't load any images", None, None)
+              SendMessage (chat.id, "Couldn't load any images", false, None, None)
             else
-              SendMessage (chat.id, List.nth !meow_cache (Random.int len), None, None)
+              SendMessage (chat.id, List.nth !meow_cache (Random.int len), false, None, None)
           end in
       let quote = function
         | {chat; reply_to_message = Some ({text = Some text} as msg)} ->
           Db.Quotes.put ~quote:text ~who:(get_sender msg) ();
-          SendMessage (chat.id, "Quoting " ^ get_sender msg ^ " who said:\n" ^ text, None, None)
+          SendMessage (chat.id, "Quoting " ^ get_sender msg ^ " who said:\n" ^ text, false, None, None)
         | {chat; reply_to_message = Some msg} ->
-          SendMessage (chat.id, "Quoting " ^ get_sender msg ^ " who said nothing", None, None)
+          SendMessage (chat.id, "Quoting " ^ get_sender msg ^ " who said nothing", false, None, None)
         | {chat} ->
           let (sender, msg, time) = Db.Quotes.get_random () in
-          SendMessage (chat.id, sender ^ " said:\n" ^ msg, None, None) in
+          SendMessage (chat.id, sender ^ " said:\n" ^ msg, false, None, None) in
       let sed = function
         | {chat; message_id; text = Some text; reply_to_message = Some {text = Some original}} ->
           let open Batteries.String in
@@ -51,8 +53,8 @@ module MyBot = Api.Mk (struct
           let (_, cmd) = split text ~by:" " in
           let (sub, by) = Tuple2.mapn strip (split cmd ~by:"/") in
           let (_, result) = replace ~str:original ~sub ~by in
-          SendMessage (chat.id, result, Some message_id, None)
-        | {chat; message_id} -> SendMessage (chat.id, "Invalid usage of /sed", Some message_id, None) in
+          SendMessage (chat.id, result, false, Some message_id, None)
+        | {chat; message_id} -> SendMessage (chat.id, "Invalid usage of /sed", false, Some message_id, None) in
       let decide = function
         | {chat; message_id; text = Some text} ->
           let open Batteries.String in
@@ -65,11 +67,11 @@ module MyBot = Api.Mk (struct
           let options = get_options [] all in
           let len = List.length options in
           if len = 0 then
-            SendMessage (chat.id, "Give me options nerd", Some message_id, None)
+            SendMessage (chat.id, "Give me options nerd", false, Some message_id, None)
           else if len = 1 then
-            SendMessage (chat.id, List.nth ["yes"; "no"] (Random.int 2), Some message_id, None)
+            SendMessage (chat.id, List.nth ["yes"; "no"] (Random.int 2), false, Some message_id, None)
           else
-            SendMessage (chat.id, List.nth options (Random.int len), Some message_id, None)
+            SendMessage (chat.id, List.nth options (Random.int len), false, Some message_id, None)
         | _ -> Nothing in
       let jukebox = function
         | {chat; text = Some text; reply_to_message = Some {audio = Some {file_id}}} -> begin
@@ -77,19 +79,19 @@ module MyBot = Api.Mk (struct
             let (_, info) = split text ~by:" " in
             let (performer, title) = split info ~by:"-" in
             Db.Jukebox.put ~title:(trim @@ title) ~performer ~file_id ();
-            SendMessage (chat.id, "Added " ^ info ^ " to the database!", None, None)
+            SendMessage (chat.id, "Added " ^ info ^ " to the database!", false, None, None)
           end
         | {chat; message_id; text = Some text} -> begin
             let open Batteries.String in
             match tokenize text with
-            | [] -> SendMessage (chat.id, concat "\n" (Db.Jukebox.list ()), None, None)
+            | [] -> SendMessage (chat.id, concat "\n" (Db.Jukebox.list ()), false, None, None)
             | xs -> begin
                 let song = trim @@ concat " " xs in
                 let (title, performer, file_id) = Db.Jukebox.search ~title:song in
-                ResendAudio (chat.id, file_id, performer, title, Some message_id, None)
+                ResendAudio (chat.id, file_id, performer, title, false, Some message_id, None)
               end
           end
-        | {chat; message_id} -> SendMessage (chat.id, "Invalid input", Some message_id, None) in
+        | {chat; message_id} -> SendMessage (chat.id, "Invalid input", false, Some message_id, None) in
       let enable = function
         | {chat; text = Some text; from} ->
           let permission = match from with
@@ -119,17 +121,17 @@ module MyBot = Api.Mk (struct
             | None -> false in
           if permission then
             Nothing (* OP the user... how do we get their user id from just a username? *)
-          else SendMessage (chat.id, "You don't have permission to OP other users", None, None)
+          else SendMessage (chat.id, "You don't have permission to OP other users", false, None, None)
         | {chat} -> Nothing in
       let share_audio song performer title = function
-        | {chat; message_id} -> ResendAudio (chat.id, song, performer, title, Some message_id, None) in
+        | {chat; message_id} -> ResendAudio (chat.id, song, performer, title, false, Some message_id, None) in
       let unfree = function
-        | {chat} -> SendVoice (chat.id, "data/free.ogg", None, None, function Success id -> Nothing
-                                                                            | Failure er -> SendMessage (chat.id, "Failed to send audio with: " ^ er, None, None)) in
+        | {chat} -> SendVoice (chat.id, "data/free.ogg", false, None, None, function Success id -> Nothing
+                                                                                   | Failure er -> SendMessage (chat.id, "Failed to send audio with: " ^ er, false, None, None)) in
       let dab = function
-        | {chat} -> Chain (SendPhoto (chat.id, "data/dab.jpg", Some "Bitch, dab", None, None, function Success id -> Nothing
-                                                                                                     | Failure er -> SendMessage (chat.id, "Failed to send photo with: " ^ er, None, None)),
-                           ResendAudio (chat.id, "BQADAQADcwADi_LrCbRvyK66JIVTAg", "Migos", "Pipe It Up", None, None)) in
+        | {chat} -> Chain (SendPhoto (chat.id, "data/dab.jpg", Some "Bitch, dab", false, None, None, function Success id -> Nothing
+                                                                                                            | Failure er -> SendMessage (chat.id, "Failed to send photo with: " ^ er, false, None, None)),
+                           ResendAudio (chat.id, "BQADAQADcwADi_LrCbRvyK66JIVTAg", "Migos", "Pipe It Up", false, None, None)) in
       [{name = "hello"; description = "Greet the user"; enabled = true; run = greet};
        {name = "meow"; description = "Load images from /r/meow_irl"; enabled = true; run = meow};
        {name = "free"; description = "Free the world from the clutches of proprietary software"; enabled = true; run = share_audio "BQADAQADcQADi_LrCWoG5Wp27N76Ag" "Richard M. Stallman" "Free Software Song"};
